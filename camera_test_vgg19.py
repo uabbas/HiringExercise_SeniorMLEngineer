824f9587a1052bcaf50c502c616aaeb7a49e6777
Ummar Abbas <ummar.abbas@gmail.com>
2017-07-31 13:48:56 +0300

    initial

---
+++ camera_test_vgg19.py
@@ -0,0 +1,81 @@
+from keras.preprocessing import image as image_utils
+from keras.applications.imagenet_utils import decode_predictions
+from keras.applications.imagenet_utils import preprocess_input
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
+from keras.applications.resnet50 import ResNet50
+import argparse
+import cv2
+import numpy as np
+import os
+import random
+import sys
+
+import threading
+
+label = ['']
+frame = None
+
+class MyThread(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+
+	def run(self):
+		#global label
+		# Load the VGG16 network
+		print("[INFO] loading network...")
+		#self.model = VGG16(weights="imagenet")
+		self.model = VGG19(weights="imagenet")
+
+		while (~(frame is None)):
+			result = self.predict(frame)
+			#if (len(result) >= 1):
+			#	label = result[1]
+
+	def predict(self, frame):
+		global label
+		image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB).astype(np.float32)
+		#image = image.transpose((2, 0, 1))
+		image = image.reshape((1,) + image.shape)
+		
+		#image = (np.array(frame).astype(np.float32)).reshape((1, 3, 224, 224))
+		
+		#image = np.expand_dims(image, axis=0)
+		#image=np.swapaxes(np.swapaxes(frame, 1, 2), 0, 1).astype(np.float32)
+		#image = np.expand_dims(image, axis=0)
+
+		image = preprocess_input(image, 'channels_last')
+		preds = self.model.predict(image)
+		decoded_preds = decode_predictions(preds)
+		first_pred = decoded_preds[0]
+		pred = first_pred[0]
+		label[0] = pred[1]
+		return label
+
+cap = cv2.VideoCapture(0)
+if (cap.isOpened()):
+	print("Camera OK")
+else:
+	cap.open()
+
+keras_thread = MyThread()
+keras_thread.start()
+
+while (True):
+	ret, original = cap.read()
+
+	frame = cv2.resize(original, (224, 224))
+#	t = MyThread()
+#	t.run()
+	# Display the predictions
+	# print("ImageNet ID: {}, Label: {}".format(inID, label))
+	cv2.putText(original, "Label: {}".format(label[0]), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+	cv2.imshow("Classification", original)
+
+	if (cv2.waitKey(1) & 0xFF == ord('q')):
+		break;
+
+cap.release()
+frame = None
+cv2.destroyAllWindows()
+sys.exit()
